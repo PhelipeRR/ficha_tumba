f782a02fd2c3a7d5a905618baaad67b1268f0ac4
PhelipeRR <phelipe.r.ribeiro@gmail.com>
2025-11-12 14:10:26 -0300

    chore: remove generated Next.js development files

--- .next/dev/static/chunks/src_app_layout_tsx_3873bb71._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__243764b8._.css",
-  "static/chunks/node_modules_tailwindcss_dist_4d86d7cd._.js"
-],
-    source: "dynamic"
-});
